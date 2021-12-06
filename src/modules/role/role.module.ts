f64f64eb7932f7985af547c15c1cd20d9892d043
Jose Miguel Duque Mejia - Ceiba Software <jose.duque@ceiba.com.co>
2021-12-06 15:13:17 -0500

    ADD: Role Controller Entity and Repository

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleRepository } from './role.repository';
+import { SharedModule } from '../../shared/shared.modules';
+import { RoleService } from './role.service';
+import { RoleController } from './role.controller';
 
-@Module({})
+@Module({
+    imports: [ TypeOrmModule.forFeature([ RoleRepository ]), SharedModule ],
+    providers: [RoleService],
+    controllers: [RoleController]
+})
 export class RoleModule {}
