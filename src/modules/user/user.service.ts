55af15de9684b1ec92978cccce2e98e88b1ef89b
Jose Miguel Duque Mejia - Ceiba Software <jose.duque@ceiba.com.co>
2021-12-06 15:30:51 -0500

    REF: Deleting mapper service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-import { MapperService } from '../../shared/mapper.service';
 import { UserDto } from './dto/user.dto';
 import { User } from './user.entity';
 import { UserDetails } from './user.details.entity';
@@ -12,42 +11,39 @@ import { Role } from '../role/role.entity';
 export class UserService {
     constructor(
         @InjectRepository( UserRepository )
-        private readonly _userRepository: UserRepository,
-        private readonly _mapperService: MapperService
+        private readonly _userRepository: UserRepository
     ) {}
 
-    async get( id: number ): Promise<UserDto> {
+    async get( id: number ): Promise<User> {
         if( ! id ) throw new BadRequestException( 'Id must be sent' );
         const user: User = await this._userRepository.findOne( id, {
             where: { status: 'ACTIVE' }
         });
         if( ! user ) throw new NotFoundException();
-        return this._mapperService.map<User, UserDto>( user, new UserDto() );
+        return user;
     }
 
-    async getAll(): Promise<UserDto[]> {
-
+    async getAll(): Promise<User[]> {
         const users: User[] = await this._userRepository.find( {
             where: { status: 'ACTIVE' }
         });
-        return this._mapperService.mapCollection<User, UserDto>(
-            users, new UserDto()
-        );
+        return users;
     }
 
-    async create( user: User ): Promise<UserDto> {
+    async create( user: User ): Promise<User> {
 
         /**
-         * @todo Hardcoded
+         * @TODO Hardcoded
          */
         const details = new UserDetails();
         user.details =  details;
+
         const repo = await getConnection().getRepository(Role);
         const defaultRole = await repo.findOne({ where: { name: 'GENERAL' } });
         user.roles = [defaultRole];
 
         const savedUser: User = await this._userRepository.save( user );
-        return this._mapperService.map<User, UserDto>( savedUser, new UserDto() );
+        return savedUser;
     }
 
     async update( id: number, user: User ): Promise<void> {
